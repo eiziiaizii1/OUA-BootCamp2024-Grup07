c9ff796f43c2e34ff45c68ff861f5aca0f6db739
Aziz Önder <azizonder05@gmail.com>
2024-07-04 13:58:09 +0300

    bunny model is added and beaver model with ainmations is added instead of rat

--- Assets/Scripts/CharacterTransformation.cs
+++ Assets/Scripts/CharacterTransformation.cs
@@ -27,7 +27,7 @@ public class CharacterTransformation : MonoBehaviour
     private readonly Dictionary<CharacterType, CharacterProperties> characterProperties = new Dictionary<CharacterType, CharacterProperties>
     {
         { CharacterType.Human,      new CharacterProperties(2f, 5.325f, 1.2f, 1f) },
-        { CharacterType.Rat,        new CharacterProperties(1.0f, 2.0f, 0.2f, 0.3f) },
+        { CharacterType.Rat,        new CharacterProperties(1.0f, 2.0f, 0.2f, 0.13f) },
         { CharacterType.Chameleon,  new CharacterProperties(1.5f, 2.5f, 1.2f, 0.5f) },
         { CharacterType.Monkey,     new CharacterProperties(3.0f, 5.0f, 2.0f, .65f) },
         { CharacterType.Kangaroo,   new CharacterProperties(2.5f, 4.5f, 3.0f, 1.5f) }
